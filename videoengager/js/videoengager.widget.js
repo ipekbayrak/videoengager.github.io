abb8ccc16fb5ee7ef92b5fcd2d702ee4b613c68f
stefandimow <stefan@videoengager.com>
2021-07-30 15:38:55 +0300

    improve the widgets/wrappers

--- videoengager/js/videoengager.widget.js
+++ videoengager/js/videoengager.widget.js
@@ -1,180 +1,110 @@
-var popupinstance = null;
-var iframeHolder = null;
-var iframeInstance;
-var oVideoEngager;
-var finalUserData = {};
-var interactionId;
-var startVideoEngager = function(url) {
-	var left = (screen.width/2)-(770/2);
-	var top = (screen.height/2)-(450/2);
-	if (!popupinstance) {
-		popupinstance = window.open(url, "popup_instance", "width=770, height=450, left=" + left + ", top=" + top + ", location=no, menubar=no, resizable=yes, scrollbars=no, status=no, titlebar=no, toolbar = no");
-	}
-	popupinstance.focus();
-};
 
-var closeIframeOrPopup = function(){
-	interactionId = null;
-	if (!iframeHolder) {
-		if (popupinstance) {
-			popupinstance.close();
-		}
-		popupinstance = null;
-	} else {
-		iframeHolder.removeChild(iframeHolder.getElementsByTagName('iframe')[0]);
-		iframeHolder.style.display = 'none';
-	}
-}
 
 var VideoEngager = function () {
-	this.init = function (callHolder, platform, tenantId, veUrl, formData, userData, audioOnly, autoAccept, hideChat) {
-		var TENANT_ID = tenantId;
-		var startWithVideo = (audioOnly) ? !audioOnly : true;
-		autoAccept = (autoAccept) ? autoAccept : true;
-		hideChat = (hideChat) ? hideChat : true;
-		
-		if (callHolder) {
-			iframeHolder = document.getElementById(callHolder);
+	var popupinstance = null;
+	var iframeHolder = null;
+	var iframeInstance;
+	var oVideoEngager;
+	var interactionId;
+	var TENANT_ID;
+	var startWithVideo;
+	var autoAccept;
+	var platform;
+	var veUrl;
+	var enablePrecall;
+	var i18n;
+	var useWebChatForm;
+	var webChatFormData;
+	var i18nDefault = { "en": { 
+		"ChatFormSubmitVideo": "Start Video",
+		"WebChatTitleVideo": "Video Chat",
+		"ChatFormSubmitAudio": "Start Audio",
+		"WebChatTitleAudio": "Audio Chat",
+		}};
+	var form;
+
+	var init = function () {
+		var config = window._genesys.widgets.videoengager;
+		TENANT_ID = config.tenantId;
+		startWithVideo = (config.audioOnly) ? !config.audioOnly : true;
+		autoAccept = (config.autoAccept) ? config.autoAccept : true;
+		platform = config.platform;
+		veUrl = config.veUrl;
+		i18n = config.i18n;
+		form = config.form;
+		enablePrecallForced = config.hasOwnProperty("enablePrecall")
+		enablePrecall = config.enablePrecall;
+		useWebChatForm = config.useWebChatForm;
+		webChatFormData = (config.webChatFormData)?config.webChatFormData: {};
+		if (config.callHolder) {
+			iframeHolder = document.getElementById(config.callHolder);
 			if (!iframeInstance) {
-				console.log("iframe holder is passing, but not found: " + callHolder);
+				console.log("iframe holder is passing, but not found: " + config.callHolder);
 			}
 		}
+	};	 
 
-		var terminateInteraction = function(){
-			//close toaster and terminate the call
-			interactionId = null;
-			oVideoEngager.command('WebChat.endChat')
-			.done(function(e){
-				oVideoEngager.command('WebChat.close');
-			})
-			.fail(function(e){
-				//
-			});
+	var startVideoEngager = function() {
+		if (interactionId == undefined) {
+			interactionId = getGuid();
+		}
+		if (useWebChatForm) {
+			initiateForm();
+		} else {
+			startWithHiddenChat();
 		}
 
-		var sendInteractionMessage = function(interactionId){
-			if (platform == 'purecloud') {
-				var firstname = $("#form_firstname").val() ? $("#form_firstname").val() : "First";
-				var lastName = $("#form_lastname").val() ? $("#form_lastname").val() : "Last";
-				var email = $("#form_email").val() ? $("#form_email").val() : "test@test";
-				var message = {interactionId:  interactionId, email: email, firstName: firstname, lastName: lastName};
-				oVideoEngager.command('WebChatService.sendMessage',{message:JSON.stringify(message)})
-				.done(function (e) {
-					console.log("send message success:" +message);
-				})
-				.fail(function(e) {
-					console.log("fail to send message: "+message);
-				});
-			}
-		}
+	}
 
-		var initiateForm = function(){
-			var fieldDefinition = {
-				wrapper: "<table></table>",
-					inputs: [
-						{ id:"form_firstname", name: "firstname", maxlength: "100", placeholder: "FirstName", label: "FirstName", autofocus: !0, "aria-required": !0 },
-						{ id:"form_lastname", name: "lastname", maxlength: "100", placeholder: "LastName", label: "LastName", "aria-required": !0 },
-						{ id:"form_email", type: "email", name: "email", maxlength: "100", placeholder: "Email", label: "Email", "aria-required": !0 },
-						// to add custom fields to web chat
-						{ value:"", name: "customField2", maxlength: "100", placeholder: "Cust", label: "Subject" },
-						{ value:"Subject", name: "customField2Label", maxlength: "100", placeholder: "", label: !1, style:"display:none" },
-						{ value:"", name: "customField1", maxlength: "100", placeholder: "Test 2", label: "BRAND NEW CUSTOM FIELD" },
-						{ value:"BRAND NEW CUSTOM FIELD", name: "customField1Label", maxlength: !1, placeholder: !1, label: !1, style:"display:none"},
-					]
-			};
+	this.initExtension = function ($, CXBus, Common) {
 
-			oVideoEngager.command('WebChat.open', {
-				userData: {},
-				//prefill values
-				form: {/*
-					autoSubmit: false,
-					firstname: 'John',
-					lastname: 'Smith',
-					email: 'John@mail.com',
-					subject: 'Customer Satisfaction'
-					*/
-				},
-				formJSON: fieldDefinition,
-				markdown: false
-			}).done(function (e2) {
-				// form opened 
-				document.getElementsByClassName("cx-submit")[0].addEventListener("click", function(){
-					startVideoChat();
-				})				
-			});
-		}
-
-		var startVideoChat = function() {
+		console.log("on init extension VideoEngager");
+		init();
+		oVideoEngager = CXBus.registerPlugin("VideoEngager");
+		oVideoEngager.publish("ready"); 
+		oVideoEngager.registerCommand("startVideo", function (e) {
+			//videochat channel is selected
+			console.log("startVideoTriggered");
+			startWithVideo = true;
+			startVideoEngager();
+		});
 		
-			var getGuid = function () {
-				function s4() {
-					return Math.floor((1 + Math.random()) * 0x10000) .toString(16) .substring(1);
-				}
-				return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
-			}
+		oVideoEngager.registerCommand("startAudio", function (e) {
+			startWithVideo = false;
+			startVideoEngager()
+		});
 
-			if (interactionId == undefined) {
-				interactionId = getGuid();
-			}
+		oVideoEngager.registerCommand("startVideoEngager", function (e) {
+			startVideoEngager()
+		});
 
-			console.log("InteractionId :", interactionId);
-			startWithVideo = startWithVideo.toString();
-			var left = (screen.width / 2) - (770 / 2);
-			var top = (screen.height / 2) - (450 / 2);
-			var str = {
-				"video_on": startWithVideo, 
-				"sessionId": interactionId, 
-				"hideChat": hideChat, 
-				"type": "initial", 
-				"defaultGroup": "floor", 
-				"view_widget": "4", 
-				"offline": true, 
-				"aa": autoAccept, 
-				skip_private: true,
-				"inichat": "false"
-			};
-	
-			var encodedString = window.btoa(JSON.stringify(str));
-			var homeURL = veUrl + '/static/';
-			var url = homeURL + 'popup.html?tennantId=' + window.btoa(TENANT_ID) + '&params=' + encodedString;
-			
-			if (!iframeHolder) {
-				if (!popupinstance) {
-					popupinstance = window.open(url, "popup_instance", "width=770, height=450, left=" + left + ", top=" + top + ", location=no, menubar=no, resizable=yes, scrollbars=no, status=no, titlebar=no, toolbar = no");
-				}
-				popupinstance.focus();
-			} else {
-				iframeInstance = document.createElement('iframe');
-				iframeInstance.width = "100%"
-				iframeInstance.height = "100%"
-				iframeInstance.id = "videoengageriframe"
-				iframeInstance.allow = "microphone; camera"
-				iframeInstance.src = url;
-				iframeHolder.insertBefore(iframeInstance, iframeHolder.firstChild);
-				iframeHolder.style.display = 'block';
+		oVideoEngager.registerCommand("endCall", function (e) {
+			oVideoEngager.command('WebChatService.endChat');
+			closeIframeOrPopup();
+		});
+
+		oVideoEngager.subscribe("WebChatService.ended", function(){
+			console.log('WebChatService.ended');
+			closeIframeOrPopup();
+		});			
+		
+		oVideoEngager.subscribe("WebChatService.started", function(){
+			console.log('WebChatService.started');
+			if (interactionId != null){
+				sendInteractionMessage(interactionId);
 			}
 			
-			window.removeEventListener('message', function (e) {});
-			window.addEventListener('message', function (event) {
-				if (event.data.type === 'popupClosed') {
-					oVideoEngager.command('WebChatService.endChat')
-					.done(function (e) {
-						console.log('WebChatService.endChat');
-					})
-					.fail(function (e) {
-						console.log('WebChatService.endChat failed');
-					});
-				}
-			});
-		};	 
+		});
+		
+		oVideoEngager.ready();
 
-		window._genesys.widgets.onReady = function(oCXBus){
+		window._genesys.widgets.onReady = function(oCXBus) {
 			console.log('[CXW] Widget bus has been initialized!');
 			oCXBus.command('WebChatService.registerPreProcessor', {preprocessor: function(oMessage){
 				if (oMessage.text && oMessage.text.indexOf(veUrl) != -1) { 
 					var url = oMessage.text;
 					oMessage.html = true;
-					oMessage.text = 'Please press button to start video:<br><br><button type="button" class="cx-btn cx-btn-primary i18n" onclick="startVideoEngager(\'' + url + '\');">Start video</button>';
+					oMessage.text = 'Please press button to start video:<br><br><button type="button" class="cx-btn cx-btn-primary i18n" onclick="startVideoEngagerOutbound(\'' + url + '\');">Start video</button>';
 					return oMessage;
 				}
 			}})
@@ -186,88 +116,218 @@ var VideoEngager = function () {
 			});
 		};
 
-		window._genesys.cxwidget = window._genesys.widgets;
-		if (!window._genesys.widgets.extensions) {
-			window._genesys.widgets.extensions = {};
-		}
-
-		window._genesys.widgets.extensions["VideoEngager"] = function ($, CXBus, Common) {
-			console.log("on init extension VideoEngager");
-			oVideoEngager = CXBus.registerPlugin("VideoEngager");
-			oVideoEngager.publish("ready"); 
-			
-			oVideoEngager.registerCommand("startVideo", function (e) {
-				//videochat channel is selected
-				console.log("startVideoTriggered");
-				startWithVideo = true;
-
-				//set language to change form language for videoengagar video chat
-				var default_lang = window._genesys.widgets.main.lang;
-				oVideoEngager.command('App.setLanguage', {lang: default_lang + "_videochat"})
-				.done(function(e){
-					// App set language successfully started
-					initiateForm();
-				})
-				.fail(function(e){
-					// App failed to set language
-				});
-			});
-			
-			oVideoEngager.registerCommand("endCall", function (e) {
-				oVideoEngager.command('WebChatService.endChat');
-				closeIframeOrPopup();
-			});
-
-			oVideoEngager.registerCommand("startAudio", function (e) {
-				startWithVideo = false;var default_lang = window._genesys.widgets.main.lang;
-				oVideoEngager.command('App.setLanguage', {lang: default_lang + "_videochat"})
-				.done(function(e){
-					initiateForm();
-				});
-			});
-			
-			oVideoEngager.subscribe("WebChatService.ended", function(){
-				console.log('WebChatService.ended');
-				closeIframeOrPopup();
-			});			
-			
-			oVideoEngager.subscribe("WebChatService.started", function(){
-				console.log('WebChatService.started');
-				if (interactionId != null){
-					sendInteractionMessage(interactionId);
-				}
-				
-			});
-			
-			oVideoEngager.ready();
-
-			//terminate call on page close
-			window.onbeforeunload = function(){
-				terminateInteraction();
-			}
-		};
-		
-		var messageHandler = function (e) {
-			console.log('messageHandler', e.data);
-			if (e.data.type === 'popupClosed') {
-				terminateInteraction();
-			}
-			if (e.data.type === 'callEnded') {
-				terminateInteraction();
-			}
-		};
-		
-		if (window.addEventListener) {
-			window.addEventListener("message", messageHandler, false);
-		} else {
-			window.attachEvent("onmessage", messageHandler);
-		}
-	
 	};
+
+	var initiateForm = function() {
+		var webChatOpenData = {
+			userData: {},
+			//prefill values
+			form: {/*
+				autoSubmit: false,
+				firstname: 'John',
+				lastname: 'Smith',
+				email: 'John@mail.com',
+				subject: 'Customer Satisfaction'
+				*/
+			},
+		}
+		if (form) {
+			webChatOpenData.formJSON = form;
+		}
+
+		oVideoEngager.command('WebChat.open', webChatOpenData)
+		.done(function (e2) {
+			// form opened 
+			document.getElementsByClassName("cx-submit")[0].addEventListener("click", function(){
+				startVideoChat();					
+			})			
+			localizeChatForm();
+	
+		});
+	}
+	var localizeChatForm = function() {
+		var lang = window._genesys.widgets.main.lang;
+		if (startWithVideo) {
+			var title = i18nDefault["en"].WebChatTitleVideo;
+			var submitButton = i18nDefault["en"].ChatFormSubmitVideo;	
+		} else {
+			var title = i18nDefault["en"].WebChatTitleAudio;
+			var submitButton = i18nDefault["en"].ChatFormSubmitAudio;	
+		}
+		if (startWithVideo) {
+			if (i18n[lang] && i18n[lang].WebChatTitleVideo) {
+				title = i18n[lang].WebChatTitleVideo;
+			} 
+			if (i18n[lang] && i18n[lang].ChatFormSubmitVideo) {
+				submitButton = i18n[lang].ChatFormSubmitVideo;
+			} 
+		} else {
+			if (i18n[lang] && i18n[lang].WebChatTitleAudio) {
+				title = i18n[lang].WebChatTitleAudio;
+			} 
+			if (i18n[lang] && i18n[lang].ChatFormSubmitAudio) {
+				submitButton = i18n[lang].ChatFormSubmitAudio;
+			} 
+
+		}
+		document.getElementsByClassName("cx-title")[0].innerHTML = title
+		document.getElementsByClassName("cx-submit")[0].innerHTML = submitButton
+	}
+
+	this.terminateInteraction = function(){
+		closeIframeOrPopup();
+		oVideoEngager.command('WebChat.endChat')
+		.done(function(e){
+			oVideoEngager.command('WebChat.close');
+		})
+		.fail(function(e){
+			//
+		});
+	}
+
+	var sendInteractionMessage = function(interactionId){
+		if (platform == 'purecloud') {
+			var message = {interactionId:  interactionId};
+			//oVideoEngager.command('WebChatService.sendFilteredMessage',{message:JSON.stringify(message), regex: /[a-zA-Z]/})
+			oVideoEngager.command('WebChatService.sendMessage',{message:JSON.stringify(message)})
+			.done(function (e) {
+				console.log("send message success:" +message);
+			})
+			.fail(function(e) {
+				console.log("fail to send message: "+message);
+			});
+		}
+	}
+
+	var startWithHiddenChat = function() {
+		if (!webChatFormData.userData) {
+			webChatFormData.userData = {};
+		}
+		if (!webChatFormData.form) {
+			webChatFormData.form = {};
+		}
+
+		webChatFormData.form.firstName = webChatFormData.firstname
+		webChatFormData.form.lastName = webChatFormData.lastname
+		webChatFormData.form.email = webChatFormData.email
+		webChatFormData.form.subject = webChatFormData.subject
+		webChatFormData.form.message = webChatFormData.message
+		webChatFormData.form.nickName = webChatFormData.nickname
+		webChatFormData.userData['veVisitorId'] = interactionId
+		startVideoChat()
+		oVideoEngager.command('WebChatService.startChat', webChatFormData)
+			.done(function (e) {
+				console.log('WebChatService started Chat');
+			}).fail(function (e) {
+				console.error("WebChatService failed to start chat: ", e);
+				closeIframeOrPopup();
+		});
+	};
+
+	var getGuid = function () {
+		function s4() {
+			return Math.floor((1 + Math.random()) * 0x10000) .toString(16) .substring(1);
+		}
+		return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+	}
+
+
+	var startVideoChat = function() {
+	
+		console.log("InteractionId :", interactionId);
+		var left = (screen.width / 2) - (770 / 2);
+		var top = (screen.height / 2) - (450 / 2);
+		var str = {
+			"video_on": startWithVideo, 
+			"sessionId": interactionId, 
+			"hideChat": true, 
+			"type": "initial", 
+			"defaultGroup": "floor", 
+			"view_widget": "4", 
+			"offline": true, 
+			"aa": autoAccept, 
+			"skip_private": true,
+			"inichat": "false"
+		};
+
+		var encodedString = window.btoa(JSON.stringify(str));
+		var homeURL = veUrl + '/static/';
+		var url = homeURL + 'popup.html?tennantId=' + window.btoa(TENANT_ID) + 
+			'&params=' + encodedString;
+		if (enablePrecallForced && enablePrecall) {
+				url+='&pcfl=true'
+		} else if (enablePrecallForced && !enablePrecall) {
+				url+='&precall=false'
+		}
+		
+		if (!iframeHolder) {
+			if (!popupinstance) {
+				popupinstance = window.open(url, "popup_instance", "width=770, height=450, left=" + left + ", top=" + top + ", location=no, menubar=no, resizable=yes, scrollbars=no, status=no, titlebar=no, toolbar = no");
+			}
+			popupinstance.focus();
+		} else {
+			iframeInstance = document.createElement('iframe');
+			iframeInstance.width = "100%"
+			iframeInstance.height = "100%"
+			iframeInstance.id = "videoengageriframe"
+			iframeInstance.allow = "microphone; camera"
+			iframeInstance.src = url;
+			iframeHolder.insertBefore(iframeInstance, iframeHolder.firstChild);
+			iframeHolder.style.display = 'block';
+		}	
+	};
+
+	var startVideoEngagerOutbound = function(url) {
+		var left = (screen.width/2)-(770/2);
+		var top = (screen.height/2)-(450/2);
+		if (!popupinstance) {
+			popupinstance = window.open(url, "popup_instance", "width=770, height=450, left=" + left + ", top=" + top + ", location=no, menubar=no, resizable=yes, scrollbars=no, status=no, titlebar=no, toolbar = no");
+		}
+		popupinstance.focus();
+	};
+
+	var closeIframeOrPopup = function(){
+		interactionId = null;
+		if (!iframeHolder) {
+			if (popupinstance) {
+				popupinstance.close();
+			}
+			popupinstance = null;
+		} else {
+			if (iframeHolder.getElementsByTagName('iframe')[0]) {
+				iframeHolder.removeChild(iframeHolder.getElementsByTagName('iframe')[0]);
+			}
+			iframeHolder.style.display = 'none';
+
+		}
+	}	
+	
+
 };
 
-var videoengager = new VideoEngager();
-window.videoengager = videoengager;
+var videoEngager = new VideoEngager();
+window.videoEngager = videoEngager;
+
+var messageHandler = function (e) {
+	console.log('messageHandler', e.data);
+	if (e.data.type === 'popupClosed') {
+		CXBus.command('VideoEngager.endCall');
+	}
+	if (e.data.type === 'callEnded') {
+		CXBus.command('VideoEngager.endCall');
+	}
+};
+
+if (window.addEventListener) {
+	window.addEventListener("message", messageHandler, false);
+} else {
+	window.attachEvent("onmessage", messageHandler);
+}
+
+//terminate call on page close
+window.onbeforeunload = function() {
+	videoEngager.terminateInteraction();
+}
 
 var eventName = 'VideoEngagerReady';
 let event;
